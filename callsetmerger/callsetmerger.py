8f649ac8b188316c34d6317e05cc96c9f2d49118
Nima Mousavi <mousavi@ucsd.edu>
2020-11-04 15:02:45 -0800

    Made the script reach the state of the previous state in printing potential merge options

--- callsetmerger/callsetmerger.py
+++ callsetmerger/callsetmerger.py
@@ -13,10 +13,12 @@ import trtools.utils.tr_harmonizer as trh
 import trtools.utils.mergeutils as mergeutils
 import trtools.utils.common as common
 
+
 class VCFWrapper:
     def __init__(self, reader, vcftype):
         self.vcftype = vcftype
         self.vcfreader = reader
+
 
 class Readers:
     def __init__(self, vcfpaths):
@@ -31,28 +33,28 @@ class Readers:
         self.chroms = list(contigs)
         self.current_records = [next(wrapper.vcfreader) for wrapper in self.vcfwrappers]
         self.done = mergeutils.DoneReading(self.current_records)
-        if not self.AreChromsValid():
+        if not self.areChromsValid():
             return 1
         self.is_min_pos_list = mergeutils.GetMinRecords(self.current_records, self.chroms)
-        self.cur_range_chrom, self.cur_range_start_pos, self.cur_range_end_pos =\
-            self.GetCurrentRange()
-        self.is_overlap_min = self.GetOverlapMinRecords()
+        self.cur_range_chrom, self.cur_range_start_pos, self.cur_range_end_pos = \
+            self.getCurrentRange()
+        self.is_overlap_min = self.getOverlapMinRecords()
 
-    def AreChromsValid(self):
+    def areChromsValid(self):
         for r, wrapper in zip(self.current_records, self.vcfwrappers):
             if r is None: continue
             if not r.CHROM in self.chroms:
                 common.WARNING((
-                "Error: found a record in file {} with "
-                "chromosome '{}' which was not found in the contig list "
-                "({})").format(wrapper.vcfreader.filename, r.CHROM, ", ".join(self.chroms)))
+                                   "Error: found a record in file {} with "
+                                   "chromosome '{}' which was not found in the contig list "
+                                   "({})").format(wrapper.vcfreader.filename, r.CHROM, ", ".join(self.chroms)))
                 return False
         return True
 
-    def GetIsMinPosList(self):
+    def getIsMinPosList(self):
         return mergeutils.GetMinRecords(self.current_records, self.chroms)
 
-    def GetCurrentRange(self):
+    def getCurrentRange(self):
         start_pos = None
         end_pos = -1
         chrom = None
@@ -60,13 +62,12 @@ class Readers:
             if self.is_min_pos_list[i]:
                 chrom = self.current_records[i].CHROM
                 start_pos = self.current_records[i].POS
-                end = start_pos + len(trh.HarmonizeRecord(self.vcfwrappers[i].vcftype, \
-                    self.current_records[i]).ref_allele)
+                end = start_pos + len(trh.HarmonizeRecord(self.vcfwrappers[i].vcftype,
+                                                          self.current_records[i]).ref_allele)
                 if end > end_pos: end_pos = end
         return chrom, start_pos, end_pos
 
-
-    def GetOverlapMinRecords(self):
+    def getOverlapMinRecords(self):
         # Set is_overlap_min for anything overlapping
         is_overlap_min = []
         for i in range(len(self.current_records)):
@@ -77,24 +78,25 @@ class Readers:
                 is_overlap_min.append(False)
                 continue
             start_pos = self.current_records[i].POS
-            end = start_pos + len(trh.HarmonizeRecord(self.vcfwrappers[i].vcftype, \
-                self.current_records[i]).ref_allele)        
+            end = start_pos + len(trh.HarmonizeRecord(self.vcfwrappers[i].vcftype,
+                                                      self.current_records[i]).ref_allele)
             if end <= self.cur_range_end_pos:
                 is_overlap_min.append(True)
-            else: is_overlap_min.append(False)
+            else:
+                is_overlap_min.append(False)
 
         return is_overlap_min
 
-    def GetMergableCalls(self):
+    def getMergableCalls(self):
         if sum(self.is_overlap_min) <= 1:
-            return None # TODO. boring for debugging. just a single genotyper
+            return None  # TODO. boring for debugging. just a single genotyper
 
         print("############")
-        print("%s:%s-%s"%(self.cur_range_chrom, self.cur_range_start_pos, self.cur_range_end_pos))
+        print("%s:%s-%s" % (self.cur_range_chrom, self.cur_range_start_pos, self.cur_range_end_pos))
         allele_list = []
         sample_calls = {}
         # Print out info
-        for i in range(len(self.current_records)):        
+        for i in range(len(self.current_records)):
             if self.is_overlap_min[i] and self.current_records[i] is not None:
                 hm = trh.HarmonizeRecord(self.vcfwrappers[i].vcftype, self.current_records[i])
                 ref = hm.ref_allele
@@ -102,34 +104,37 @@ class Readers:
                 allele_list.append((ref, "*", self.vcfwrappers[i].vcftype, "GT:0", repunit))
                 altnum = 1
                 for alt in hm.alt_alleles:
-                    allele_list.append((alt, len(alt)-len(ref), self.vcfwrappers[i].vcftype, "GT:%s"%altnum, repunit))
+                    allele_list.append(
+                        (alt, len(alt) - len(ref), self.vcfwrappers[i].vcftype, "GT:%s" % altnum, repunit))
                     altnum += 1
                 for sample in self.current_records[i]:
                     if sample.sample not in sample_calls: sample_calls[sample.sample] = []
-                    if sample is None or not sample.called: 
+                    if sample is None or not sample.called:
                         sample_calls[sample.sample].append(([None, None], self.vcfwrappers[i].vcftype))
                         continue
                     sample_calls[sample.sample].append((sample["GT"], self.vcfwrappers[i].vcftype))
-        if len(allele_list) == sum(self.is_overlap_min): return None # TODO boring for debugging. all ref
+        if len(allele_list) == sum(self.is_overlap_min): return None  # TODO boring for debugging. all ref
 
         for a in allele_list:
             print(a)
         for s in sample_calls:
             print(s)
             for call in sample_calls[s]:
-                print("%s: %s"%(call[1], call[0]))
+                print("%s: %s" % (call[1], call[0]))
         return allele_list, sample_calls
 
-    def GoToNext(self):
-        self.current_records = mergeutils.GetNextRecords([w.vcfreader for w in self.vcfwrappers], self.current_records, self.GetIsMinPosList())
+    def goToNext(self):
+        self.current_records = mergeutils.GetNextRecords([w.vcfreader for w in self.vcfwrappers], self.current_records,
+                                                         self.getIsMinPosList())
         # after increment, check if chroms valid
-        if not self.AreChromsValid():
+        if not self.areChromsValid():
             return 1
         self.done = mergeutils.DoneReading(self.current_records)
         self.is_min_pos_list = mergeutils.GetMinRecords(self.current_records, self.chroms)
-        self.cur_range_chrom, self.cur_range_start_pos, self.cur_range_end_pos =\
-            self.GetCurrentRange()
-        self.is_overlap_min = self.GetOverlapMinRecords()
+        self.cur_range_chrom, self.cur_range_start_pos, self.cur_range_end_pos = \
+            self.getCurrentRange()
+        self.is_overlap_min = self.getOverlapMinRecords()
+
 
 class MergeObject:
     def __init__(self, allele_list, sample_calls):
@@ -138,22 +143,29 @@ class MergeObject:
 
     def merge(self):
         pass
-        
+
+
 def main():
     parser = argparse.ArgumentParser(__doc__)
-    parser.add_argument("--vcfs", help="Comma-separated list of VCFs to merge. Must be sorted/indexed", type=str, required=True)
+    parser.add_argument("--vcfs", help="Comma-separated list of VCFs to merge. Must be sorted/indexed", type=str,
+                        required=True)
     args = parser.parse_args()
 
     readers = Readers(args.vcfs.split(","))
-
 
     # Check samples same in each VCF
     # TODO
 
     # Walk through sorted readers
     while not readers.done:
-        # Get mergable calls
-        allele_list, sample_calls = readers.GetMergableCalls()
+        # Get mergeable calls
+        res = readers.getMergableCalls()
+        if res is not None:
+            allele_list, sample_calls = res
+        else:
+            # Move on
+            readers.goToNext()
+            continue
 
         # Merge calls
         mo = MergeObject(allele_list, sample_calls)
@@ -161,8 +173,8 @@ def main():
         # TODO will need to update is_min_pos_list if we merged something
 
         # Move on
-        readers.GoToNext()
+        readers.goToNext()
+
 
 if __name__ == "__main__":
     main()
-
