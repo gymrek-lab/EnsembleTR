881cfed001666fe62755f060c65a67d8e99869fa
gymreklab <melissa.gymrek@gmail.com>
2021-01-27 10:34:20 -0800

    got compareSTR test working

--- callset-qc/run_cap.sh
+++ callset-qc/run_cap.sh
@@ -3,6 +3,8 @@
 VCFLIST=$1
 OUTDIR=$2
 
+FAI=/storage/resources/dbase/human/GRCh38/GRCh38_full_analysis_set_plus_decoy_hla.fa.fai
+
 mkdir -p ${OUTDIR}/tmp
 
 # First, load capillary data from spreadsheet
@@ -24,21 +26,31 @@ do
     if [[ $numsamp -gt 0 ]]
     then
 	intersectBed -header -a $vcf -b ${OUTDIR}/tmp/capillary_regions.bed | \
-	    vcf-subset -c ${OUTDIR}/tmp/${vcfname}.samples.keep | \
-	    bgzip -c > ${OUTDIR}/tmp/${vcfname}.vcf.gz
+	    vcf-subset -c ${OUTDIR}/tmp/${vcfname}.samples.keep > ${OUTDIR}/tmp/${vcfname}.vcf
+	bgzip -f ${OUTDIR}/tmp/${vcfname}.vcf
 	tabix -p vcf -f ${OUTDIR}/tmp/${vcfname}.vcf.gz
     fi
 done
 
 # Merge to a single VCF
-# TODO
+mergevcfs=$(ls ${OUTDIR}/tmp/*.vcf.gz | awk '{print $0","}' | tr -d '\n' | sed 's/,$//')
+mergeSTR --vcfs $mergevcfs --out ${OUTDIR}/tmp/merged
+bgzip -f ${OUTDIR}/tmp/merged.vcf
+tabix -p vcf -f ${OUTDIR}/tmp/merged.vcf.gz
 
 # Third, convert capillary data to a VCF file in
 # HipSTR-like format that can be read by compareSTR
-# TODO
+./convert_cap.py ${OUTDIR}/tmp/capillary.tab ${OUTDIR}/tmp/merged.vcf.gz > ${OUTDIR}/tmp/capillary.vcf
+vcf-sort ${OUTDIR}/tmp/capillary.vcf | bgzip -c > ${OUTDIR}/tmp/capillary.sorted.vcf.gz
+tabix -p vcf -f ${OUTDIR}/tmp/capillary.sorted.vcf.gz
+bcftools reheader -f $FAI -o ${OUTDIR}/tmp/capillary.sorted.reheader.vcf.gz ${OUTDIR}/tmp/capillary.sorted.vcf.gz
+tabix -p vcf -f  ${OUTDIR}/tmp/capillary.sorted.reheader.vcf.gz
 
 # Fourth, run compareSTR
-# TODO
-
+compareSTR \
+    --vcf1 ${OUTDIR}/tmp/merged.vcf.gz \
+    --vcf2 ${OUTDIR}/tmp/capillary.sorted.reheader.vcf.gz \
+    --vcftype2 gangstr \
+    --out ${OUTDIR}/comparestr
 
 echo "working on it"
